5a0c3236021062da5ff2d68c4bcba97fc21eac38
Juan Fernando <juanfed12345@gmail.com>
2023-05-07 18:31:13 -0500

    credencialers firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,15 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyA_n-F31-LDb98rf_ogvScVE2dGyOF0l3k",
+  authDomain: "file-manager-51d7b.firebaseapp.com",
+  projectId: "file-manager-51d7b",
+  storageBucket: "file-manager-51d7b.appspot.com",
+  messagingSenderId: "867036861720",
+  appId: "1:867036861720:web:4512708c58f737c9f9ba6d"
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
 
 export default fire;
+
