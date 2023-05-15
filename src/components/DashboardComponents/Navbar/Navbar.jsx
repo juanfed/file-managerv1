d0070a47b3fe1c64580e9b3fcdd770ec34ff96c7
Juan Fernando <juanfed12345@gmail.com>
2023-05-14 20:51:02 -0500

    Incribir operador, solicitar documento

--- src/components/DashboardComponents/Navbar/Navbar.jsx
+++ src/components/DashboardComponents/Navbar/Navbar.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
 import { useSelector, useDispatch } from "react-redux";
-import { SignOutUser } from "../../../redux/actionCreators/authActionCreator";
+import { Modal, Button, Form } from "react-bootstrap";
+
+import FormularioSolicitudDocumento from "../../FormularioSolicitudDocumento";
+
 
 const Navbar = () => {
   const { isAuthenticated, user } = useSelector((state) => state.auth);
+  const [showModal, setShowModal] = useState(false); // Definimos el estado para el modal del formulario de solicitar documento
+  const [showModalIncribir, setShowModalIncribir] = useState(false)
   const dispatch = useDispatch();
+
+  const handleCloseModal = () => setShowModal(false); // Función para cerrar el modal
+  const handleShowModal = () => setShowModal(true); // Función para mostrar el modal
+
+  const handleCloseModalIncribir = () => setShowModalIncribir(false)
+  const handleShowModalIncribir = () => setShowModalIncribir(true)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes añadir la lógica para enviar la información del formulario a la base de datos
+    handleCloseModalIncribir();
+  };
+
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm  p-3">
@@ -21,12 +39,13 @@ const Navbar = () => {
                 <span className="fw-bold">{user.displayName}</span>
               </p>
             </li>
-            <li className="nav-item">
-              <button
-                className="btn btn-secondary"
-              >
-                Solicitar Documento
+            <li className="nav-item mx-2">
+              <button className="nav-link btn btn-primary" onClick={handleShowModalIncribir}>
+                Inscribir operador
               </button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-secondary" onClick={handleShowModal}>Solicitar Documento</button>
             </li>
             <li className="nav-item mx-2">
               <Link className="btn btn-primary" to="/">
@@ -57,6 +76,49 @@ const Navbar = () => {
           </>
         )}
       </ul>
+      <Modal show={showModal} onHide={handleCloseModal}> {/* Aquí definimos el modal */}
+        <Modal.Header closeButton>
+          <Modal.Title>Solicitud de Documento</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <FormularioSolicitudDocumento />
+        </Modal.Body>
+      </Modal>
+
+
+      <Modal show={showModalIncribir} onHide={handleCloseModalIncribir}>
+        <Modal.Header closeButton>
+          <Modal.Title>Inscribir Operador</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formNumeroCedula">
+              <Form.Label>Numero de Cedula</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese el numero de cedula" />
+            </Form.Group>
+
+            <Form.Group controlId="formCampoTexto">
+              <Form.Label>Campo de Texto</Form.Label>
+              <Form.Control type="text" placeholder="Ingrese el texto" />
+            </Form.Group>
+
+            <Form.Group controlId="formOperadores">
+              <Form.Label>Lista de Operadores</Form.Label>
+              <Form.Control as="select" defaultValue="Seleccione un operador...">
+                <option>Operador 1</option>
+                <option>Operador 2</option>
+                <option>Operador 3</option>
+                <option>Operador 4</option>
+                <option>Operador 5</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </nav>
   );
 };
---
+++ src/components/FormularioSolicitudDocumento.jsx
@@ -0,0 +1,34 @@
+import Navbar from '../components/DashboardComponents/Navbar/Navbar';
+import React, { Component } from 'react';
+
+class FormularioSolicitudDocumento extends Component {
+  render() {
+    return (
+      <div className="container mt-3">
+        <h1>Solicitar Documento</h1>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="asunto" className="form-label">Asunto</label>
+            <input type="text" className="form-control" id="asunto" />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="entidadesPublicas" className="form-label">Entidades Públicas</label>
+            <select className="form-select" id="entidadesPublicas">
+              <option selected>Seleccione una opción</option>
+              <option value="1">Entidad Pública 1</option>
+              <option value="2">Entidad Pública 2</option>
+              <option value="3">Entidad Pública 3</option>
+            </select>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="mensaje" className="form-label">Mensaje</label>
+            <textarea className="form-control" id="mensaje" rows="3"></textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Enviar</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormularioSolicitudDocumento;
